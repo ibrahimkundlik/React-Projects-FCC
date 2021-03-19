e3fd8ac2aa23648c97ce355279e8432f0962d4ed
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-03-19 23:43:55 +0530

    Added a few more variable in SASS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Reminder from "./components/reminder/reminder.component";
+import Tours from "./components/tours/tours.component";
 
 function App() {
 	return (
 		<div className="App">
-			<Reminder />
+			{/* <Reminder /> */}
+			<Tours />
 		</div>
 	);
 }
