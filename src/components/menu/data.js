74f6de7a608cf0bd5b68c351a9ce37652e0d5b8c
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-03-22 16:02:04 +0530

    Worked on Menu Component

--- src/components/menu/data.js
+++ src/components/menu/data.js
@@ -4,7 +4,7 @@ const menu = [
 		title: "buttermilk pancakes",
 		category: "breakfast",
 		price: 15.99,
-		img: "./assets/item-1.jpeg",
+		img: "./assets/menu/item-1.jpeg",
 		desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
 	},
 	{
@@ -12,7 +12,7 @@ const menu = [
 		title: "diner double",
 		category: "lunch",
 		price: 13.99,
-		img: "./assets/item-2.jpeg",
+		img: "./assets/menu/item-2.jpeg",
 		desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
 	},
 	{
@@ -20,7 +20,7 @@ const menu = [
 		title: "godzilla milkshake",
 		category: "shakes",
 		price: 6.99,
-		img: "./assets/item-3.jpeg",
+		img: "./assets/menu/item-3.jpeg",
 		desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
 	},
 	{
@@ -28,7 +28,7 @@ const menu = [
 		title: "country delight",
 		category: "breakfast",
 		price: 20.99,
-		img: "./assets/item-4.jpeg",
+		img: "./assets/menu/item-4.jpeg",
 		desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
 	},
 	{
@@ -36,7 +36,7 @@ const menu = [
 		title: "egg attack",
 		category: "lunch",
 		price: 22.99,
-		img: "./assets/item-5.jpeg",
+		img: "./assets/menu/item-5.jpeg",
 		desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
 	},
 	{
@@ -44,7 +44,7 @@ const menu = [
 		title: "oreo dream",
 		category: "shakes",
 		price: 18.99,
-		img: "./assets/item-6.jpeg",
+		img: "./assets/menu/item-6.jpeg",
 		desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
 	},
 	{
@@ -52,7 +52,7 @@ const menu = [
 		title: "bacon overflow",
 		category: "breakfast",
 		price: 8.99,
-		img: "./assets/item-7.jpeg",
+		img: "./assets/menu/item-7.jpeg",
 		desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
 	},
 	{
@@ -60,7 +60,7 @@ const menu = [
 		title: "american classic",
 		category: "lunch",
 		price: 12.99,
-		img: "./assets/item-8.jpeg",
+		img: "./assets/menu/item-8.jpeg",
 		desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
 	},
 	{
@@ -68,7 +68,7 @@ const menu = [
 		title: "quarantine buddy",
 		category: "shakes",
 		price: 16.99,
-		img: "./assets/item-9.jpeg",
+		img: "./assets/menu/item-9.jpeg",
 		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
 	},
 ];
--- src/components/menu/menu.component.jsx
+++ src/components/menu/menu.component.jsx
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./menu.styles.scss";
 import data from "./data";
 
 const Menu = () => {
-	return <div>Menu Component{console.log(data)}</div>;
+	const [menu, setMenu] = useState(data);
+	const [active, setActive] = useState("all");
+	const handleChange = (cat) => {
+		if (cat === "all") {
+			setMenu(data);
+			setActive("all");
+		} else {
+			setMenu(data.filter((item) => item.category === cat));
+			setActive(cat);
+		}
+	};
+
+	return (
+		<div className="menu-cont">
+			<h1>Our Menu</h1>
+			<div className="underline"></div>
+			<ul className="nav-links">
+				<li
+					className={active === "all" ? `active` : ``}
+					onClick={() => handleChange("all")}
+				>
+					All
+				</li>
+				<li
+					className={active === "breakfast" ? `active` : ``}
+					onClick={() => handleChange("breakfast")}
+				>
+					Breakfast
+				</li>
+				<li
+					className={active === "lunch" ? `active` : ``}
+					onClick={() => handleChange("lunch")}
+				>
+					Lunch
+				</li>
+				<li
+					className={active === "shakes" ? `active` : ``}
+					onClick={() => handleChange("shakes")}
+				>
+					Shakes
+				</li>
+			</ul>
+			<div className="items-cont">
+				{menu.map(({ id, title, price, img, desc }) => (
+					<div className="item" key={id}>
+						<div
+							className="item-img"
+							style={{
+								backgroundImage: `url(${img})`,
+							}}
+						></div>
+						<div className="title-price">
+							<p className="item-title">{title}</p>
+							<p className="item-price">${price}</p>
+						</div>
+						<p className="item-desc">{desc}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Menu;
